5a3d367f9624f1b873de91dbaea4776e98a21fd9
Erez Hazak <erhazak@cisco.com>
2018-08-28 22:05:25 -0400

    Small change to table naming Fix crontab issue Fix start script port not parsing properly (dash vs. bash)

---
+++ scripts/collect_images.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /home/pi/picture_frame
+python collect_images.py
---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+cd /home/pi/picture_frame
+port=$(echo `grep tcp_port config.json` | cut -d" " -f2)
+sudo gunicorn -b 0.0.0.0:$port -w 2 picture_frame:app &
