bc9d64c17f20ff42ca20432d761bd731dd30b26f
Erez Hazak <erhazak@cisco.com>
2020-07-16 10:54:13 -0400

    Cleaned code and left only the picture frame code

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
 from app import app
 from flask import render_template, request, url_for, redirect, flash
-from app.forms import ShowUpdateForm, AddShowForm
 import json
 from random import choice
-from utils import load_shows_data, load_external_conf, update_show, sort_shows, add_show_to_list, delete_show, \
-    load_chores_data, day_of_week, hour_of_day, create_viewed_images_file
+from utils import load_external_conf, create_viewed_images_file
 from pathlib import Path
 
 
@@ -37,68 +35,3 @@ def index():
         break
 
     return render_template('index.html', image=image_to_show)
-
-
-@app.route('/calendar')
-def calendar():
-    config = load_external_conf()
-    calendar_url = config['google_calendar']
-    return render_template('calendar.html', calendar_url=calendar_url, port=config['tcp_port'],
-                           zipcode=config['zipcode'])
-
-
-@app.route('/showsBacklog', methods=['GET', 'POST'])
-def shows_backlog():
-    config = load_external_conf()
-    form = ShowUpdateForm()
-
-    if request.method == 'POST' and form.validate_on_submit():
-        show = form.sname.data
-        delete = form.delete.data
-
-        print("Delete:{}".format(delete))
-        if len(show) == 0:
-            flash("Must choose a show!")
-        elif delete:
-            delete_show(show)
-        else:
-            update_show(show)
-        return redirect(url_for('shows_backlog'))
-
-    shows = load_shows_data()
-    shows = sort_shows(shows)
-    return render_template('shows_backlog.html', shows=shows, form=form, request=request, port=config['tcp_port'])
-
-
-@app.route('/addShow', methods=['GET', 'POST'])
-def add_show():
-    form = AddShowForm()
-
-    if request.method == 'POST':
-        show = form.sname.data
-        swatch = form.swatch.data
-        show_type = int(form.show_type.data)
-        if len(show) == 0:
-            flash('No show added, please type show name')
-            return redirect(url_for('add_show'))
-        elif show_type == 2 and swatch != 0:
-            flash("A movie can't have seasons, please set seasons watched to 0")
-            return redirect(url_for('add_show'))
-        else:
-            add_show_to_list(show, swatch, show_type)
-        return redirect(url_for('shows_backlog'))
-
-    return render_template('add_show.html', form=form, request=request)
-
-
-@app.route('/chores')
-def chores():
-    chores_list = load_chores_data()
-    hour = hour_of_day()
-    if hour < 12:
-        meal = "b"
-    elif 17 > hour > 12:
-        meal = "l"
-    else:
-        meal = "d"
-    return render_template('chores.html', chores=chores_list[day_of_week()], meal=meal)
--- utils.py
+++ utils.py
@@ -1,77 +1,10 @@
 import json
-from datetime import datetime
-
-
-def day_of_week():
-    days = {0: "monday", 1: "tuesday", 2: "wednesday", 3: "thursday", 4: "friday", 5: "saturday", 6: "sunday"}
-    today = datetime.today().weekday()
-    return days[today]
-
-
-def hour_of_day():
-    return datetime.today().hour
-
-
-def sort_shows(shows):
-    return sorted(shows, key=lambda k: k.get('priority', 0), reverse=False)
 
 
 def load_external_conf():
     with open("config.json") as h:
         config = json.loads(h.read())
     return config
-
-
-def load_shows_data():
-    with open('app/templates/shows.json') as h:
-        return json.loads(h.read())
-
-
-def load_chores_data():
-    with open('app/templates/chores.json') as h:
-        return json.loads(h.read())
-
-
-def write_shows_data(shows):
-    with open('app/templates/shows.json', "w")as h:
-        json.dump(shows, h)
-
-
-def update_show(show_name):
-    shows = load_shows_data()
-    new_shows = []
-    max_priority = max([show['priority'] for show in shows])
-    for show in shows:
-        if show['name'] == show_name:
-            swatched = show['season_watched'] + 1
-            priority = max_priority + 1
-            show_type = show['type']
-            new_shows.append({"name": show_name, "season_watched": swatched, "priority": priority, "type": show_type})
-        else:
-            new_shows.append(show)
-    write_shows_data(new_shows)
-
-
-def add_show_to_list(show_name, season_watched, show_type):
-    shows = load_shows_data()
-
-    min_priority = min([show['priority'] for show in shows])
-    for show in shows:
-        show['priority'] += 1
-    new_show = {"name": show_name, "priority": min_priority, "season_watched": season_watched, "type": show_type}
-    shows.append(new_show)
-    write_shows_data(shows)
-
-
-def delete_show(name):
-    shows = load_shows_data()
-    new_shows = []
-    for show in shows:
-        if show['name'] == name:
-            continue
-        else:
-            new_shows.append(show)
-    write_shows_data(new_shows)
 
 
 def create_viewed_images_file(filename):
